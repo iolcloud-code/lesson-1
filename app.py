7bcdf0be6a74c8f24ffcc619803a0f9bb312a2af
Javier Cristobal <javier.cristobal.toscano@gmail.com>
2023-06-16 13:31:47 +0200

    app.run from iol-invoice-adaptable

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ app = Flask(__name__)
 def index():
     return 'My first flask Project'
 
-def app():
-    app.run(host='0.0.0.0', port=$PORT)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=3000)
