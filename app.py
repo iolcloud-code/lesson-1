bfcc86fdb2985bdb64bc29b19f2c51fb0c541d8d
Javier Cristobal <javier.cristobal.toscano@gmail.com>
2023-06-16 13:41:41 +0200

    port=3000

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'My first flask Project'
+    return 'My first flask App'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=3000)
